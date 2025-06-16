3edc30fea8fd809d66b7a7050765036eabc9843c
Nazarii Khalimonov <nazar131188@gmail.com>
2025-06-16 16:21:00 +0300

    dev

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,11 @@
-# write your code here
+from django.urls import include, path
+from rest_framework.authtoken import views
+
+from user.views import CreateUserView, LoginUserView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="user_register"),
+    path("login/", LoginUserView.as_view(), name="user_login"),
+]
+
+app_name = "user"
